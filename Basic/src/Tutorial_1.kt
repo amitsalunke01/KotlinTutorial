8fcf59ec6beee3bc031af59a77e05b25b5e13c81
amitsalunke01 <amitsalunke01@gmail.com>
2020-06-06 03:16:14 +0530

    updated file with variables and primitive type

--- Basic/src/Tutorial_1.kt
+++ Basic/src/Tutorial_1.kt
@@ -1,3 +1,12 @@
-fun main(){
-    println("Hello world amit salunke")
+fun main() {
+    val userName: String = "amit salunke"
+    var age = 31//Type Inference
+    Int
+    println("Hello world $userName. I am $age years old. In two years I would be ${age + 2}")
+    helloWorld()
+}
+
+
+fun helloWorld() {
+    print("Welcome to kotlin")
 }
